49cfd54146f3c4de6d50f18f6797cad99cf45607
Filippo Gasparini <filippogaspa02@gmail.com>
2024-11-20 17:04:25 +0100

    ADD: correzione

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-console.log('array')
+
 
 const posts = [
     {
--- script.js
+++ script.js
@@ -5,7 +5,19 @@
 const express = require('express')
 const router = express.router
 const postsController = require('./controllers')
+const post = require('./data')
+const port = 3000
 
 
+// lista dei post
 
 router.get('/', postsController.index)
+
+// richiamo un singolo post
+
+router.get('/post', postsControllers.show)
+
+// elimino un singolo post dalla lista
+
+router.get('/delete', postsControllers.destroy)
+

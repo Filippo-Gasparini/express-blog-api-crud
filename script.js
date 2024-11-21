cbcbeee781d16f510f8651082e45372cb786060e
Filippo Gasparini <filippogaspa02@gmail.com>
2024-11-21 17:21:14 +0100

    ADD: correzione

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-//console.log('ok')
+
 
 // richiamo express e il file router
-
 const express = require('express')
 const app = express()
+
+// richiamo l'api con la porta
 const port = 3000
 
+
 const postRouter = require('./router/posts');
+
 
 app.use(express.json()); // Interpreta la mia richiesta come JSON
 app.use('/posts', postRouter);
 
-
+// live server
 app.listen(port, () => {
     console.log("Server started...")
 })
